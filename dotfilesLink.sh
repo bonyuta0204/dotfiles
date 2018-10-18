44d244b45b5a4334b32f08bcedfb094df0859f82
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2018-10-18 05:07:00 +0000

    refactor dotfile directory

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
-ln -sfi ~/dotfiles/vimrc ~/.vimrc
-ln -sfi ~/dotfiles/gvimrc ~/.gvimrc
-# ln -sf ~/dotfiles/.vim ~/.vim
+ln -sfi ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sfi ~/dotfiles/vim/gvimrc ~/.gvimrc
 ln -sfi ~/dotfiles/bashrc ~/.bashrc
 ln -sfi ~/dotfiles/bash_profile ~/.bash_profile
 
 SNIPPET_DIR=~/.vim/bundle/neosnippet-snippets
+VIM_DIR=~/.vim
 
+
+# vim after directory should be placed on
+# e.g. ~/dotfiles/after/ftplugin/python.vim
+if [ -d ${VIM_DIR} > /dev/null ]; then
+  ln -sdfi ~/dotfiles/vim/after ${VIM_DIR}
+fi
+
+
+# snippets files should place on
+# e.g. ~/dotfiles/snippets/ruby.snip
 if [ -d ${SNIPPET_DIR} > /dev/null ]; then
   rm -rf ${SNIPPET_DIR}/snippets
   ln -sdfi ~/dotfiles/snippets ${SNIPPET_DIR}
 fi
+
 
 # make symbolic link for fish config file
 fish_symlink(){
@@ -18,10 +29,11 @@ fish_symlink(){
   local fishroot
   dotroot="$HOME/dotfiles/config"
   fishroot="$HOME/.config"
-  ln -sfi "$dotroot/fish/config.fish" "$fishroot/fish/config.fish" 
-  ln -sfi "$dotroot/fish/fishfile" "$fishroot/fish/fishfile" 
-  ln -sdfi "$dotroot/fish/functions" "$fishroot/fish" 
+  ln -sfi "$dotroot/fish/config.fish" "$fishroot/fish/config.fish"
+  ln -sfi "$dotroot/fish/fishfile" "$fishroot/fish/fishfile"
+  ln -sdfi "$dotroot/fish/functions" "$fishroot/fish"
 }
+
 
 if command -v fish > /dev/null ; then
   fish_symlink
---
+++ vim/after/ftplugin/python.vim
@@ -0,0 +1,9 @@
+inoremap <buffer> __ ____<LEFT><LEFT>
+inoremap <buffer> """
+\ """"""<LEFT><LEFT><LEFT><CR><ESC>O
+inoremap <buffer> == <space>==<space>
+inoremap <buffer> , ,<space>
+
+setlocal foldmethod=indent
+setlocal expandtab
+setlocal foldlevel=99
