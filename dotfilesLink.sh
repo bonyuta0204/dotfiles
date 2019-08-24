233af6b7473c6f1b43745b10090e2e7c83b2afe0
Yuta Nakamura <nakamurayuta0204@gmail.com>
2019-08-24 21:11:11 +0900

    fix dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,34 +1,47 @@
 #!/bin/sh
+
+# always use `ln target link_name` format
+# for example
+# ln vimrc $HOME/.vimrc
+# ln vim/ $HOME/.vim
+# do not use `ln target directory` format 
+# like `ln vim/ $HOME/`
+
+# Options are different between linux and BSD(mac)
+
+LN_OPTS="-sfT"
+
+
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
-ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/bashrc ~/.bashrc
-ln -sf ~/dotfiles/bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln $LN_OPTS ~/dotfiles/vim/gvimrc ~/.gvimrc
+ln $LN_OPTS ~/dotfiles/bashrc ~/.bashrc
+ln $LN_OPTS ~/dotfiles/bash_profile ~/.bash_profile
+ln $LN_OPTS ~/dotfiles/tmux.conf ~/.tmux.conf
 
 
-SNIPPET_DIR=~/.vim/dein/repos/github.com/Shougo/neosnippet-snippets/snippets
-VIM_DIR=~/.vim
+SNIPPET_DIR="~/.vim/dein/repos/github.com/Shougo/neosnippet-snippets/snippets"
+VIM_DIR="~/.vim"
 
 
 # vim setting
 if [ -d $HOME/.vim/dein > /dev/null ]; then
   mkdir -p $HOME/.vim/dein
-  ln -sf ~/dotfiles/vim/plugins.toml ~/.vim/dein/plugins.toml
-  ln -sf ~/dotfiles/vim/lazy.toml ~/.vim/dein/lazy.toml
+  ln $LN_OPTS ~/dotfiles/vim/plugins.toml ~/.vim/dein/plugins.toml
+  ln $LN_OPTS ~/dotfiles/vim/lazy.toml ~/.vim/dein/lazy.toml
 fi
 
 
 # vim after directory should be placed on
 # e.g. ~/dotfiles/after/ftplugin/python.vim
 if [ -d ${VIM_DIR} > /dev/null ]; then
-  ln -sfh ~/dotfiles/vim/after "${VIM_DIR}/after"
+  ln $LN_OPTS ~/dotfiles/vim/after "${VIM_DIR}/after"
 fi
 
 
 # snippets files should place on
 # e.g. ~/dotfiles/snippets/ruby.snip
 if [ -d ${SNIPPET_DIR} > /dev/null ]; then
-  ln -sfh ~/dotfiles/vim/snippets ${SNIPPET_DIR}
+  ln $LN_OPTS ~/dotfiles/vim/snippets ${SNIPPET_DIR}
 fi
 
 
@@ -37,9 +50,9 @@ if command -v nvim > /dev/null ; then
   if [ ! -d $HOME/.config/nvim ]; then
     mkdir -p $HOME/.config/nvim
   fi
-  ln -sfh ~/.vim ~/.config/nvim
-  ln -sfh ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
-  ln -sfh ~/dotfiles/vim/ale_linters ~/.vim/ale_linters
+  ln $LN_OPTS ~/.vim ~/.config/nvim
+  ln $LN_OPTS ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
+  ln $LN_OPTS ~/dotfiles/vim/ale_linters ~/.vim/ale_linters
 fi
 
 
@@ -49,12 +62,12 @@ fish_symlink(){
   local fishroot
   dotroot="$HOME/dotfiles/config"
   fishroot="$HOME/.config"
-  ln -sf "$dotroot/fish/config.fish" "$fishroot/fish/config.fish"
-  ln -sf "$dotroot/fish/fishfile" "$fishroot/fish/fishfile"
+  ln $LN_OPTS "$dotroot/fish/config.fish" "$fishroot/fish/config.fish"
+  ln $LN_OPTS "$dotroot/fish/fishfile" "$fishroot/fish/fishfile"
   if [ ! -d $HOME/.config/fish/functions > /dev/null ]; then
     mkdir -p "$HOME/.config/fish/functions"
   fi
-  ln -sf $HOME/dotfiles/config/fish/functions/* "$HOME/.config/fish/functions"
+  ln $LN_OPTS $HOME/dotfiles/config/fish/functions/* "$HOME/.config/fish/functions"
 }
 
 
