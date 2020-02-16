eac65fb1d8599ceced9cfc26faa7172169fbd8f6
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2020-02-17 01:00:34 +0900

    fix dein test

--- test/vim/main.sh
+++ test/vim/main.sh
@@ -4,7 +4,7 @@
 ## check if dein install is succeed
 
 echo "testing if dein is correctly installed"
-if [ -d ~/.vim/dein/.cache/.vimrc/.dein ];then
+if [ -d ~/.vim/dein/.cache/.vimrc/.dein/pluginp ];then
   echo "OK"
 else
   echo "NG"
