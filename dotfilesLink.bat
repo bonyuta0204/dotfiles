71650e166c3cdd40639b6ba8e4fe601a4a883ec4
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2020-02-17 02:06:42 +0900

    add link batch

--- dotfilesLink.bat
+++ dotfilesLink.bat
@@ -1,5 +1,14 @@
 @echo off
-echo %HOMEDRIVE%%HOMEPATH%"\dotfiles\colors"
-mklink %HOMEPATH%"\_vimrc" %HOMEDRIVE%%HOMEPATH%"\dotfiles\.vimrc"
-mklink /D %HOMEPATH%"\vimfiles\colors" %HOMEDRIVE%%HOMEPATH%"\dotfiles\colors\"
-mklink %HOMEPATH%"\_gvimrc" %HOMEDRIVE%%HOMEPATH%"\dotfiles\.gvimrc"
+
+call :force_link %HOMEPATH%"\.vimrc" %HOMEDRIVE%%HOMEPATH%"\dotfiles\vim\vimrc"
+call :force_link %HOMEPATH%"\.gvimrc" %HOMEDRIVE%%HOMEPATH%"\dotfiles\vim\gvimrc"
+call :force_link %HOMEPATH%"\.bashrc" %HOMEDRIVE%%HOMEPATH%"\dotfiles\bashrc"
+call :force_link %HOMEPATH%"\.bash_profile" %HOMEDRIVE%%HOMEPATH%"\dotfiles\bash_profile"
+
+exit /b
+
+:force_link
+del %1
+mklink %1 %2
+
+exit /b
