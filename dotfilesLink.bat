9349d4b492f8a29cf7ee1699d38c5926b44395be
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2020-02-17 02:15:47 +0900

    add batch script

--- dotfilesLink.bat
+++ dotfilesLink.bat
@@ -1,6 +1,8 @@
 @echo off
 
 call :force_link %HOMEPATH%"\.vimrc" %HOMEDRIVE%%HOMEPATH%"\dotfiles\vim\vimrc"
+call :force_link %HOMEPATH%"\.vim\dein\plugins.toml" %HOMEDRIVE%%HOMEPATH%"\dotfiles\vim\plugins.toml"
+call :force_link %HOMEPATH%"\.vim\dein\lazy.toml" %HOMEDRIVE%%HOMEPATH%"\dotfiles\vim\lazy.toml"
 call :force_link %HOMEPATH%"\.gvimrc" %HOMEDRIVE%%HOMEPATH%"\dotfiles\vim\gvimrc"
 call :force_link %HOMEPATH%"\.bashrc" %HOMEDRIVE%%HOMEPATH%"\dotfiles\bashrc"
 call :force_link %HOMEPATH%"\.bash_profile" %HOMEDRIVE%%HOMEPATH%"\dotfiles\bash_profile"
