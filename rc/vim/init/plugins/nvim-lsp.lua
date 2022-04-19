751f1a3c78ca1001be0bf52c1a5431feb7c38f56
Yuta Nakamura <nakamurayuta0204@gmail.com>
2022-04-20 02:18:25 +0900

    fix: vimの設定ファイルの不具合修正

--- rc/vim/init/plugins/nvim-lsp.lua
+++ rc/vim/init/plugins/nvim-lsp.lua
@@ -34,3 +34,15 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
 end
+
+-- nvim-lsp-installer settings
+-- @see https://github.com/williamboman/nvim-lsp-installer
+-- @see https://zenn.dev/nazo6/articles/c2f16b07798bab
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+  local opts = {}
+  opts.on_attach = on_attach
+
+  server:setup(opts)
+  vim.cmd [[ do User LspAttachBuffers ]]
+end)
