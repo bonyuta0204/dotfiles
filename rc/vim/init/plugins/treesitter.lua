83e3d31fcf75e32e0b2cb0b5730a8d5f92cbc5c4
Yuta Nakamura <nakamurayuta0204@gmail.com>
2022-11-26 22:54:41 +0900

    fix: treesitterにpugを追加

--- rc/vim/init/plugins/treesitter.lua
+++ rc/vim/init/plugins/treesitter.lua
@@ -5,7 +5,7 @@ end
 
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = {"lua", "ruby", "typescript", "javascript", "vue", "css"},
+  ensure_installed = {"lua", "ruby", "typescript", "javascript", "vue", "css", "pug"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
