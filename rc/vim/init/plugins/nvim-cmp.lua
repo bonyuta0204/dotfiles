0fe3c2e503677fca9f20c7166b2eefac7e66123c
Yuta Nakamura <nakamurayuta0204@gmail.com>
2022-11-26 23:39:38 +0900

    fix: nvim-cmpのデフォルトのキーバインディングが効かない問題を解消

--- rc/vim/init/plugins/nvim-cmp.lua
+++ rc/vim/init/plugins/nvim-cmp.lua
@@ -13,28 +13,24 @@ cmp.setup({
       vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
     end,
   },
-  mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    -- Accept currently selected item. If none selected, `select` first item.
-    -- Set `select` to `false` to only confirm explicitly selected items.
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-      end
-    end, { "i", "s" })
-  },
+  mapping = cmp.mapping.preset.insert(
+    {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4)),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4)),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete()),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        end
+      end, { "i", "s" })
+    }
+  ),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
   }, {
