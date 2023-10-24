2088958e6d854f2eee27c1bd90cba6104cda143d
bonyuta0204 <nakamurayuta0204@gmail.com>
2023-10-25 00:10:34 +0900

    Fix/add fish install (#48)
    * feat: fish installation
    
    * feat: fish test
    
    * fix: some more tests
    
    * fix
    
    * fix: ubuntu installer
    
    * fix: fisher installer

---
+++ installers/fish.fish
@@ -0,0 +1,8 @@
+#!/bin/fish
+
+## Setup fish shell
+## Fish shell should be installed before running this script
+
+### Install fisher
+### fisher_plugins file should be placed right place in advance
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
