85a86c38ceade1bc630db738adcae5d86153a5fa
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-06-08 22:22:05 +0900

    Add launchd to brew bundle

--- setups/homebrew/setup.sh
+++ setups/homebrew/setup.sh
@@ -2,6 +2,25 @@
 #
 set -eu
 
+BREWFILE="$HOME/Brewfile"
+PLISTFILE="$HOME/Library/LaunchAgents/com.bonyuta0204.brew.plist"
+
+# Check if brew is installed
 if command -v brew > /dev/null; then
-  ln -sf $(realpath Brewfile) ~/Brewfile
+  # Create symlink for Brewfile
+  ln -sf "$(realpath Brewfile)" "$BREWFILE"
+  
+  # Check if the system is macOS
+  if [[ "$(uname)" == "Darwin" ]]; then
+    # Create symlink for plist and load LaunchAgent
+    ln -sf "$(realpath brew.plist)" "$PLISTFILE"
+    launchctl unload "$PLISTFILE"
+    launchctl load "$PLISTFILE"
+  else
+    echo "This script is intended to create symlink for Brewfile on non-macOS systems."
+  fi
+else
+  echo "Homebrew is not installed."
+  exit 1
 fi
+
